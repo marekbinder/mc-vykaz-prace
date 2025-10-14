a35a363c9f51fee6d10c681f1ef148d45c70bc54
marekbinder <binder.marek@gmail.com>
2025-10-14 22:09:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,12 @@ function showLogin(){
   document.getElementById('sendLink').onclick = async () => {
     const email = document.getElementById('email').value.trim()
     if(!email) return alert('Zadej e‑mail')
-    const { error } = await state.sb.auth.signInWithOtp({ email })
+    const url = window.location.origin + window.location.pathname; // např. https://uzivatel.github.io/repo/
+const { error } = await state.sb.auth.signInWithOtp({
+  email,
+  options: { emailRedirectTo: url }
+})
+
     if(error) return alert(error.message)
     alert('Zkontroluj schránku, poslal jsem odkaz.')
   }
